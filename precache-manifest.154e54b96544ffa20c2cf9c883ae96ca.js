095dd792f725a21226caaa307f50a338d37cebdd
matrix112 <ibcherb@gmail.com>
2021-11-01 23:04:35 +1300

    Updates

---
+++ precache-manifest.154e54b96544ffa20c2cf9c883ae96ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011981ed0b3128a0280499ba65135de8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07a36a1e53df2c56806b",
+    "url": "/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "ff21febe7a1c3e88b929",
+    "url": "/static/css/main.d6c2332f.chunk.css"
+  },
+  {
+    "revision": "07a36a1e53df2c56806b",
+    "url": "/static/js/2.4c6eff5e.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.4c6eff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff21febe7a1c3e88b929",
+    "url": "/static/js/main.e233cb4d.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
